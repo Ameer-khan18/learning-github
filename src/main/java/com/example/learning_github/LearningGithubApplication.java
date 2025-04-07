c5fa28edda53c9ea9bb78c7ca301e8c4036e72cd
unknown <ameersalmankhancse@gmail.com>
2025-04-07 04:35:00 -0700

    Initial commit

---
+++ src/main/java/com/example/learning_github/LearningGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learning_github/LearningGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
